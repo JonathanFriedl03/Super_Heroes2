655f3525868886b157b39fa5fc24fa0c563ea90a
Jonathan Friedl <jfreetz2@gmail.com>
2020-04-20 13:56:14 -0500

    initial commit

---
+++ SuperHeroes2/Models/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroes2.Models
+{
+    public class SuperHero
+    {
+       
+    }
+}
